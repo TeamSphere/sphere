4cf32d9b8145b7a6cd254bec753a1300b0c9c6c1
josephizatt <joe@izatt.co.uk>
2023-02-13 20:46:29 +0000

    Merkle trees

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require github.com/stretchr/testify v1.8.1
 
 require (
+	github.com/cbergoon/merkletree v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- types/block_test.go
+++ types/block_test.go
@@ -4,9 +4,26 @@ import (
 	"testing"
 
 	"github.com/TeamSphere/sphere/crypto"
+	"github.com/TeamSphere/sphere/proto"
 	"github.com/TeamSphere/sphere/util"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCalculateRootHash(t *testing.T) {
+	var (
+		privKey = crypto.GeneratePrivateKey()
+		block   = util.RandomBlock()
+		tx      = &proto.Transaction{
+			Version: 1,
+		}
+	)
+
+	block.Transactions = append(block.Transactions, tx)
+	SignBlock(privKey, block)
+	assert.True(t, VerifyRootHash(block))
+	assert.Equal(t, 32, len(block.Header.RootHash))
+
+}
 
 func TestSignVerifyBlock(t *testing.T) {
 	var (
