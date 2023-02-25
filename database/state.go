8f8a5508c67222d9de770b31b86e068dc30ae814
josephizatt <joe@izatt.co.uk>
2023-02-25 14:51:06 +0000

    i love you @beaa

---
+++ database/state.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+)
+
+type State struct {
+	Balances  map[Account]uint
+	txMempool []Tx
+
+	dbFile *os.File
+}
+
+func NewStateFromDisk() (*State, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	genFilePath := filepath.Join(cwd, "database", "genesis.json")
+	if err != nil {
+		return nil, err
+	}
+
+	balances := make(map[Account]uint)
+	for account, balance := range gen.Balances {
+		balances[account] = balance
+	}
+
+	txDbFilePath := filepath.Join(cwd, "database", "tx.db")
+	f, err := os.OpenFile(txDbFilePath, os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(f)
+
+}
