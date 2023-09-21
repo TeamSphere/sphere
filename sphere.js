ca2fd4d5409730dc3b5bbbf639ae20b0f6fafb22
Joseph <joe@izatt.co.uk>
2023-09-21 13:27:26 +0100

    as i go along, i will leave traces of thoughts

---
+++ sphere.js
@@ -0,0 +1,12 @@
+anyway
+import { ThemeProviderasNextThemesProvider } from 'next-themes';
+
+
+
+
+my guy is gonna think about ThemeProviderasNextThemesProvider
+what are you thinking about next joe?
+
+can you ask me a good question..ThemeProviderasNextThemesProvider
+
+?!?!
