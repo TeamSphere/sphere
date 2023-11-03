b3354a64e3de79beebc937637e6af7deea403525
Joseph <joe@izatt.co.uk>
2023-11-03 12:23:52 +0000

    Update authcontext

--- frontend/src/app/context/AuthContext.tsx
+++ frontend/src/app/context/AuthContext.tsx
@@ -1,22 +1,27 @@
-'use client'
-
-import React, { createContext, useState } from 'react';
 import axios from 'axios';
+import React, { createContext, useState, useContext } from 'react';
 
-export const AuthContext = createContext();
+type AuthContextType = {
+  user: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  register: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+};
 
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<string | null>(null);
 
   const API_BASE_URL = 'https://sentience-399715.ew.r.appspot.com/api';
 
-  const login = async (email, password) => {
+  const login = async (email: string, password: string) => {
     const res = await axios.post(`${API_BASE_URL}/login`, { username: email, password });
     localStorage.setItem('token', res.data);
     setUser(email);
   };
 
-  const register = async (email, password) => {
+  const register = async (email: string, password: string) => {
     const res = await axios.post(`${API_BASE_URL}/register`, { username: email, password });
     localStorage.setItem('token', res.data);
     setUser(email);
@@ -32,4 +37,12 @@ export const AuthProvider = ({ children }) => {
       {children}
     </AuthContext.Provider>
   );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 };
