1e268eb198f7f43bd6110eea80b7e5ec48960326
Joseph <joe@izatt.co.uk>
2023-11-03 12:20:27 +0000

    Update

--- frontend/src/app/context/AuthContext.tsx
+++ frontend/src/app/context/AuthContext.tsx
@@ -8,14 +8,16 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const API_BASE_URL = 'https://sentience-399715.ew.r.appspot.com/api';
+
   const login = async (email, password) => {
-    const res = await axios.post('http://localhost:8000/api/login', { username: email, password });
+    const res = await axios.post(`${API_BASE_URL}/login`, { username: email, password });
     localStorage.setItem('token', res.data);
     setUser(email);
   };
 
   const register = async (email, password) => {
-    const res = await axios.post('http://localhost:8000/api/register', { username: email, password });
+    const res = await axios.post(`${API_BASE_URL}/register`, { username: email, password });
     localStorage.setItem('token', res.data);
     setUser(email);
   };
